47144cf79a66671cfaa6a2ab1129a3867835c3fe
pamsler <56963839+pamsler@users.noreply.github.com>
2025-09-15 12:27:11 +0200

    Update hash.js

--- hash.js
+++ hash.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
-import bcrypt from "bcryptjs";
+import argon2 from "argon2";
 
 const password = process.argv[2];
 if (!password) {
-  console.error("Please provide a password as an argument");
+  console.error("Please enter your password as an argument.");
   process.exit(1);
 }
 
-const hash = bcrypt.hashSync(password, 12);
-console.log(hash.replace(/\$/g, "$$$$"));
+const hash = await argon2.hash(password, {
+  type: argon2.argon2id,
+  memoryCost: 19456,
+  timeCost: 3,
+  parallelism: 1
+});
+
+console.log(hash);
